9989b8adcceaf91089e4ea553fa9ca3c3f304318
Olek <olek.russ@hotmail.com>
2016-06-13 13:28:24 +0300

    make it better

--- marinehow/admin.py
+++ marinehow/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Question, Answer
+from .models import Usuario, Question, Answer, Comment
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Comment)
